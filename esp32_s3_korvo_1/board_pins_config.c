1ad643f0f028f9a9b5ce113b6d388d0cc152bb44
fat-fred <fatfred.iphone@googlemail.com>
2024-11-22 22:32:17 +0100

    Update board_pins_config.c

--- esp32_s3_korvo_1/board_pins_config.c
+++ esp32_s3_korvo_1/board_pins_config.c
@@ -50,8 +50,8 @@ esp_err_t get_i2c_pins(i2c_port_t port, i2c_config_t *i2c_config) {
 esp_err_t get_i2s_pins(i2s_port_t port, i2s_pin_config_t *i2s_config) {
   AUDIO_NULL_CHECK(TAG, i2s_config, return ESP_FAIL);
   if (port == I2S_NUM_0) {
-    i2s_config->bck_io_num = GPIO_NUM_41;
-    i2s_config->ws_io_num = GPIO_NUM_40;
+    i2s_config->bck_io_num = GPIO_NUM_40;
+    i2s_config->ws_io_num = GPIO_NUM_41;
     i2s_config->data_out_num = GPIO_NUM_39;
     i2s_config->data_in_num = GPIO_NUM_NC;
     i2s_config->mck_io_num = GPIO_NUM_42;
